04be7f471c4aeafe30b63c02ce614df122273c57
tomas96ech <techeverri96@gmail.com>
2024-05-19 19:40:05 -0500

    Added styles
    Added shadow as a border into pokemon preview
    Modified text properties into pokemon preview
    Modified sprite position

--- react_pokedex/src/components/PokemonList.jsx
+++ react_pokedex/src/components/PokemonList.jsx
@@ -6,7 +6,7 @@ import PokemonPreview from "./PokemonPreview"
 const PokemonList = ({ pokemons }) => {
     return (
         //Para generar la grilla se pone grid, para darle columnas ponemos grid-cols y le dimos parametros personalizados, repeat para que las columnas se fueran generando, con la configuración de auto-fit para que llenen el espacioi disponible y con un minimo de 180px y un maximo de 1 fracción
-        <section className="pd-4 grid grid-cols-[repeat(auto-fit,_minmax(180px,_1fr))] gap-y-14">
+        <section className="pd-4 pt-14 grid grid-cols-[repeat(auto-fit,_minmax(180px,_1fr))] gap-4 gap-y-14">
             {
                 pokemons.map((pokemon) => (
                     <PokemonPreview key={pokemon.url} pokemonURL={pokemon.url} />
--- react_pokedex/src/components/PokemonPreview.jsx
+++ react_pokedex/src/components/PokemonPreview.jsx
@@ -13,15 +13,15 @@ const PokemonPreview = ({ pokemonURL }) => {
 
 
     return (
-        <article className="text-center bg-white rounded-[30px] border-2 border-black relative">
+        <article className="capitalize text-center bg-white rounded-[30px] relative font-semibold pb-2 shadow-md shadow-slate-400">
             <header className="h-8">
                 <img src={pokemon?.sprites.front_default} alt="" className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 top-0"/>
             </header>
-            <span>{pokemon?.id}</span>
-            <h2 className="capitalize">{pokemon?.name}</h2>
-            <ul>
+            <span className="text-sm text-slate-400">N° {pokemon?.id}</span>
+            <h2 className="text-lg">{pokemon?.name}</h2>
+            <ul className="flex justify-center gap-2">
                 {pokemon?.types.map(type => (
-                <li className="capitalize" key={type.type.name}>{type.type.name}</li>
+                <li key={type.type.name}>{type.type.name}</li>
                 ))}
             </ul>
         </article>
